e8dc6b348e40f67d9c8871b2749702f4b8207631
shreshthkr <shreshthkumar6@gmail.com>
2023-04-07 18:30:10 +0530

    added tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <h1 className="text-blue-500 text-4xl font-bold">
+       Dmas Game
+    </h1>
     </div>
   );
 }
