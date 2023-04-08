a44b4c02f691f54c7a00f5fad432cef3742647e7
prateek-bka <prateek.bka@gmail.com>
2023-04-08 21:01:05 +0530

    added backend

---
+++ backend/model/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String },
+    email: { type: String },
+    password: { type: String },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
